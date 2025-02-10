2fb0fc31be86155e7bbcf77c0f74b6bf46e8c516
ChiefSpuddy <sammaxwellmay@gmail.com>
2025-02-10 16:33:51 +0000

    Settings Screen Removed

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/collections_screen.dart';
 import 'screens/search_screen.dart';
-import 'screens/settings_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/analytics_screen.dart';
 import 'screens/scanner_screen.dart';
@@ -12,7 +11,6 @@ class AppRoutes {
   static const String home = '/';
   static const String collection = '/collection';
   static const String search = '/search';
-  static const String settings = '/settings';
   static const String profile = '/profile';
   static const String analytics = '/analytics';
   static const String scanner = '/scanner';
@@ -23,7 +21,6 @@ class AppRoutes {
     collection: (context) => const CollectionsScreen(),
     analytics: (context) => const AnalyticsScreen(),
     search: (context) => const SearchScreen(),
-    settings: (context) => const SettingsScreen(),
     profile: (context) => const ProfileScreen(),
     collectionIndex: (context) => const CollectionIndexScreen(), // Add this line
     scanner: (context) => const ScannerScreen(),
