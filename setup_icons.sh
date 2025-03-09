e1bae4cffc9c1d6daa112c092b1519cba6f3c99c
ChiefSpuddy <sammaxwellmay@gmail.com>
2025-03-09 15:03:32 +0000

    1.1 Round 2

--- setup_icons.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p /Users/sam.may/CardWizz/assets/icons
-touch /Users/sam.may/CardWizz/assets/icons/.gitkeep
-echo "Created icons directory"
-chmod +x /Users/sam.may/CardWizz/setup_icons.sh
